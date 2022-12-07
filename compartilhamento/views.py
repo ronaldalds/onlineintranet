5a10bd4f4226be85e431884e874f4ce52173062b
Ronald ralds <ronald.ralds@gmail.com>
2022-12-06 23:18:18 -0300

    view mapa

--- compartilhamento/views.py
+++ compartilhamento/views.py
@@ -1,4 +1,7 @@
+import os
+
 import folium
+from django.conf import settings
 from django.shortcuts import render
 
 # Create your views here.
@@ -19,9 +22,12 @@ def mapa(request):
     center_lng = -5.176168
     center_lat = -40.680138
     map = folium.Map(
+        width='80%', height='80%',
         location=[center_lat, center_lng],
         zoom_start=8,
     )
-    map.save()
+    map.add_child(folium.LatLngPopup())
+    map.save(os.path.join(
+        settings.TEMPLATES[0]['DIRS'][0], 'partials/map.html'))
 
-    return render(request, 'map.html')
+    return render(request, 'projeto-rede-mapa.html')
