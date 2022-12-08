cde43ad7093e455d73db2762a19fb55618cb38a9
Ronald ralds <ronald.ralds@gmail.com>
2022-12-08 18:05:55 -0300

    ajustando grupos de view

--- compartilhamento/views.py
+++ compartilhamento/views.py
@@ -1,9 +1,12 @@
+import io
 import os
 
+import branca
 import folium
+from branca.element import Element, Figure
 from django.conf import settings
 from django.shortcuts import render
-from folium import plugins
+from folium.plugins import BeautifyIcon, Draw, MiniMap
 
 from . import models
 
@@ -23,16 +26,45 @@ def descricao(request):
 
 def mapa(request):
     queryPolyline = models.Trajeto.objects.all()
-    queryPop = models.Ponto.objects.filter(nome__icontains="POP")
-    queryPosteEnel = models.Ponto.objects.filter(tipo__nome__icontains="POSTE")
+    queryPop = models.Ponto.objects.filter(tipo__nome__icontains="POP")
+    queryPosteEnel = models.Ponto.objects.filter(
+        tipo__nome__icontains="POSTE ENEL")
+    base = (-5.176168, -40.680138)
+    pontos = folium.FeatureGroup('Pontos',)
+    linhas = folium.FeatureGroup('Linhas',)
+    # metro = folium.FeatureGroup('Metro')
+    # poste = folium.FeatureGroup('Poste')
+    # bkb = folium.FeatureGroup('BkB')
+    # bkh = folium.FeatureGroup('BkH')
+    # varjota = folium.FeatureGroup('Varjota')
 
-    print(queryPosteEnel)
-    base = (-4.193510, -40.476361)
     m = folium.Map(
         width='100%', height='100%',
         location=base,
         zoom_start=18,
+        tiles=None,
     )
+
+    folium.raster_layers.TileLayer(
+        tiles='http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
+        attr='google',
+        name='Google Satélite',
+        max_zoom=20,
+        subdomains=['mt0', 'mt1', 'mt2', 'mt3'],
+        overlay=False,
+        control=True,
+    ).add_to(m)
+
+    folium.raster_layers.TileLayer(
+        tiles='http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}',
+        attr='google',
+        name='Google Maps',
+        max_zoom=20,
+        subdomains=['mt0', 'mt1', 'mt2', 'mt3'],
+        overlay=False,
+        control=True,
+    ).add_to(m)
+
     for i in queryPop:
         folium.Marker(
             (i.latitude, i.longitude),
@@ -40,37 +72,39 @@ def mapa(request):
             tooltip=i.nome,
             icon=folium.Icon(color="red", icon="home", prefix='fa'),
 
-        ).add_to(m)
+        ).add_to(pontos)
+
     for i in queryPosteEnel:
         folium.Marker(
             (i.latitude, i.longitude),
             popup=i.descricao,
             tooltip=i.nome,
-            icon=plugins.BeautifyIcon(
+            icon=BeautifyIcon(
                 icon_shape='rectangle-dot',
                 border_width=5,
             ),
 
-        ).add_to(m)
+        ).add_to(pontos)
+    cabos = []
     for i in queryPolyline:
         for t in i.ligacao.all():
-            folium.PolyLine(
-                ((t.ponto_a.latitude, t.ponto_a.longitude),
-                 (t.ponto_b.latitude, t.ponto_b.longitude)),
-                popup=i.nome,
-                tooltip=i.nome,
-                color="cyan",
-                weight=6,
-            ).add_to(m)
+            cabos.append([
+                (t.ponto_a.latitude, t.ponto_a.longitude),
+                (t.ponto_b.latitude, t.ponto_b.longitude)
+            ])
 
-    # map.add_child(folium.LatLngPopup())
-    # plugins.Draw(export=True,
-    #              filename='my_data.geojson',
-    #              position='topleft',
-    #              draw_options={'polyline': {'allowIntersection': True}},
-    #              edit_options={'poly': {'allowIntersection': True}}
-    #              ).add_to(m)
-    # plugins.LocateControl().add_to(m)
+    folium.PolyLine(
+        cabos,
+        popup=folium.Html('<a href="#">asdf</a>'),
+        tooltip=i.nome,
+        color="cyan",
+        weight=6,
+        dash_array='5,15',
+    ).add_to(linhas)
+    pontos.add_to(m)
+    linhas.add_to(m)
+    MiniMap(position='bottomleft').add_to(m)
+    folium.LayerControl().add_to(m)
     m.save(os.path.join(
         settings.TEMPLATES[0]['DIRS'][0], 'partials/map.html'))
 
